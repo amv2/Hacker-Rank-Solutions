5ebe187e08bed746ec06881f92bd4178cf70272b
amv2 <93527621+amv2@users.noreply.github.com>
2022-07-19 17:47:53 +0400

    Create pangrams.py

---
+++ week2/pangrams.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    # create a set of lowercase characters
+    # from the given string
+    # if the length of this set == 26 return pangram
+    if len(set(s.lower()) - set(' ')) == 26:
+        return "pangram"
+    else:
+        return "not pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
